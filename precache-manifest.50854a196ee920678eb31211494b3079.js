bdb69dfdf7e29f8081052a99b2a57ec5457e0192
ashutosh2466 <82325928+ashutosh2466@users.noreply.github.com>
2021-05-29 16:03:47 +0530

    Updates

---
+++ precache-manifest.50854a196ee920678eb31211494b3079.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "05371ee16c310d1db3b0",
+    "url": "/ashutosh-port/static/css/main.6595163e.chunk.css"
+  },
+  {
+    "revision": "05371ee16c310d1db3b0",
+    "url": "/ashutosh-port/static/js/main.cf62d463.chunk.js"
+  },
+  {
+    "revision": "3cd548e6d722ed7c6696",
+    "url": "/ashutosh-port/static/js/runtime~main.be9ddc3b.js"
+  },
+  {
+    "revision": "9b96abdffaaa98a3f366",
+    "url": "/ashutosh-port/static/js/2.c8442eb2.chunk.js"
+  },
+  {
+    "revision": "9101a0103cf354451f9bf4d155976a88",
+    "url": "/ashutosh-port/static/media/ashu.9101a010.jpg"
+  },
+  {
+    "revision": "b4942451ce8f927bdcb80b79bc76c17a",
+    "url": "/ashutosh-port/static/media/as.b4942451.jpg"
+  },
+  {
+    "revision": "a0099b89d679ede0a79caf2a3adf31cf",
+    "url": "/ashutosh-port/static/media/blind.a0099b89.jfif"
+  },
+  {
+    "revision": "159589ce831269a32c136a846fe8d3f4",
+    "url": "/ashutosh-port/static/media/snake.159589ce.jpg"
+  },
+  {
+    "revision": "17ecd100217e1af41bc537b589ff0803",
+    "url": "/ashutosh-port/static/media/smi.17ecd100.jpg"
+  },
+  {
+    "revision": "9cd0f06a687047f48d249218e5d35d97",
+    "url": "/ashutosh-port/static/media/ic.9cd0f06a.jpg"
+  },
+  {
+    "revision": "14ac8685fd8692f9ba7ca71cc54005ac",
+    "url": "/ashutosh-port/index.html"
+  }
+];
